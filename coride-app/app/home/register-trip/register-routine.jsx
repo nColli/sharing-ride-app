66cc2730d35fc48c824f9fb570896b8b263e3acd
ncolli <collinicolas01@gmail.com>
2025-05-23 12:11:25 -0300

    guardar hora en rutina

--- coride-app/app/home/register-trip/register-routine.jsx
+++ coride-app/app/home/register-trip/register-routine.jsx
@@ -1,4 +1,11 @@
-import { View, Text, Button, Appearance } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Appearance,
+  Alert,
+  ScrollView,
+} from "react-native";
 import { useTrip } from "./TripContext";
 import { useRouter } from "expo-router";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
@@ -25,13 +32,13 @@ export default function RegisterRoutine() {
   const [dateStart, setDateStart] = useState(new Date());
   const [dateEnd, setDateEnd] = useState(new Date());
   const [days, setDays] = useState([]);
-  //LA HORA VA A SER ALMACENADA JUNTO A DATESTART Y END, LA HORA DE ESA FECHA VA A SER LA HORA ELEGIDA EN TIMESTART... PARA QUE ESTO SUCEDA CUANDO PONGO CONTINUAR DEBE CONCATENARSE
-  //O SOBREESCRIBIR LA HORA QUE ESTA EN DATESTART Y END PERO NO SE VE CON LA HORA ELEGIDA EN TIMESTART
   const [showTimePicker, setShowTimePicker] = useState(false);
   const [timeStart, setTimeStart] = useState(new Date());
 
   const onChangeDateStart = (event) => {
     setShowDatePickerStart(false);
+
+    console.log("change dateStart", event);
 
     if (event) {
       setDateStart(event);
@@ -41,6 +48,8 @@ export default function RegisterRoutine() {
   const onChangeTime = (event) => {
     setShowTimePicker(false);
 
+    console.log("change time", event);
+
     if (event) {
       setTimeStart(event);
     }
@@ -48,6 +57,8 @@ export default function RegisterRoutine() {
 
   const onChangeDateEnd = (event) => {
     setShowDatePickerEnd(false);
+
+    console.log("change dateEnd", event);
 
     if (event) {
       setDateEnd(event);
@@ -67,10 +78,29 @@ export default function RegisterRoutine() {
 
   const handleRegistrarDistancia = () => {
     //guardar en trip
+    //la hora y min de comienzo del viaje lo guardo en la hora y minutos de dateStart y dateEnd
+    if (days.length === 0) {
+      console.log("no selecciono ningun dia");
+
+      Alert.alert(
+        "Error",
+        "Selecciona los dias que queres que se repita la rutina",
+      );
+      return;
+    }
+
+    const copyDateStart = dateStart;
+    copyDateStart.setHours(timeStart.getHours());
+    copyDateStart.setMinutes(timeStart.getMinutes());
+
+    const copyDateEnd = dateEnd;
+    copyDateEnd.setHours(timeStart.getHours());
+    copyDateEnd.setMinutes(timeStart.getMinutes());
+
     const newTrip = {
       ...trip,
-      dateStart: dateStart,
-      dateEnd: dateEnd,
+      dateStart: copyDateStart,
+      dateEnd: copyDateEnd,
       isRoutine: true,
       days,
     };
@@ -83,79 +113,81 @@ export default function RegisterRoutine() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Registrar rutina</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Registrar rutina</Text>
 
-      <Text style={styles.label}>Fecha de inicio de la rutina</Text>
-      <Button
-        title={formatDate(dateStart)}
-        onPress={() => setShowDatePickerStart(true)}
-      />
-      <DateTimePickerModal
-        isVisible={showDatePickerStart}
-        time={dateStart}
-        mode="date"
-        onConfirm={onChangeDateStart}
-        onCancel={() => setShowDatePickerStart(false)}
-        isDarkModeEnabled={Appearance.getColorScheme() === "light"}
-      />
+        <Text style={styles.label}>Fecha de inicio de la rutina</Text>
+        <Button
+          title={formatDate(dateStart)}
+          onPress={() => setShowDatePickerStart(true)}
+        />
+        <DateTimePickerModal
+          isVisible={showDatePickerStart}
+          time={dateStart}
+          mode="date"
+          onConfirm={onChangeDateStart}
+          onCancel={() => setShowDatePickerStart(false)}
+          isDarkModeEnabled={Appearance.getColorScheme() === "light"}
+        />
 
-      <Text style={styles.label}>Fecha de fin de la rutina</Text>
-      <Button
-        title={formatDate(dateEnd)}
-        onPress={() => setShowDatePickerEnd(true)}
-      />
-      <DateTimePickerModal
-        isVisible={showDatePickerEnd}
-        time={dateEnd}
-        mode="date"
-        onConfirm={onChangeDateEnd}
-        onCancel={() => setShowDatePickerEnd(false)}
-        isDarkModeEnabled={Appearance.getColorScheme() === "light"}
-      />
+        <Text style={styles.label}>Fecha de fin de la rutina</Text>
+        <Button
+          title={formatDate(dateEnd)}
+          onPress={() => setShowDatePickerEnd(true)}
+        />
+        <DateTimePickerModal
+          isVisible={showDatePickerEnd}
+          time={dateEnd}
+          mode="date"
+          onConfirm={onChangeDateEnd}
+          onCancel={() => setShowDatePickerEnd(false)}
+          isDarkModeEnabled={Appearance.getColorScheme() === "light"}
+        />
 
-      <Text style={styles.label}>Selecciona la hora del viaje:</Text>
-      <Button
-        title={formatTime(timeStart)}
-        onPress={() => setShowTimePicker(true)}
-      />
-      <DateTimePickerModal
-        isVisible={showTimePicker}
-        time={timeStart}
-        mode="time"
-        onConfirm={onChangeTime}
-        onCancel={() => setShowTimePicker(false)}
-        isDarkModeEnabled={Appearance.getColorScheme() === "light"}
-      />
+        <Text style={styles.label}>Selecciona la hora del viaje:</Text>
+        <Button
+          title={formatTime(timeStart)}
+          onPress={() => setShowTimePicker(true)}
+        />
+        <DateTimePickerModal
+          isVisible={showTimePicker}
+          time={timeStart}
+          mode="time"
+          onConfirm={onChangeTime}
+          onCancel={() => setShowTimePicker(false)}
+          isDarkModeEnabled={Appearance.getColorScheme() === "light"}
+        />
 
-      <MultiSelect
-        style={styles.dropdown}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        search
-        data={data}
-        labelField="label"
-        valueField="value"
-        placeholder="Select item"
-        searchPlaceholder="Search..."
-        value={days}
-        onChange={(item) => {
-          setDays(item);
-        }}
-        renderLeftIcon={() => (
-          <AntDesign
-            style={styles.icon}
-            color="black"
-            name="Safety"
-            size={20}
-          />
-        )}
-        selectedStyle={styles.selectedStyle}
-      />
+        <MultiSelect
+          style={styles.dropdown}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          search
+          data={data}
+          labelField="label"
+          valueField="value"
+          placeholder="Seleccionar días"
+          searchPlaceholder="Buscar..."
+          value={days}
+          onChange={(item) => {
+            setDays(item);
+          }}
+          renderLeftIcon={() => (
+            <AntDesign
+              style={styles.icon}
+              color="black"
+              name="Safety"
+              size={20}
+            />
+          )}
+          selectedStyle={styles.selectedStyle}
+        />
 
-      <Button title="Continuar" onPress={handleRegistrarDistancia} />
-    </View>
+        <Button title="Continuar" onPress={handleRegistrarDistancia} />
+      </View>
+    </ScrollView>
   );
 }
